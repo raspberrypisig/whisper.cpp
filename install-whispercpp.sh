2c2303ef5069bd37923474ef32d0b606b18b90be
raspberrypisig <raspberrypisig@gmail.com>
2022-12-08 18:35:51 +1100

    Update install-whispercpp.sh

--- install-whispercpp.sh
+++ install-whispercpp.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 set -x
 cd ~
-sudo apt install -y libcurlpp-dev libopenblas-dev cmake git
+sudo apt install -y libsdl2-dev libcurlpp-dev libopenblas-dev cmake git wget
 git clone https://github.com/ggerganov/whisper.cpp 
 cd whisper.cpp
-bash ./models/download-ggml-model.sh base.en
 bash ./models/download-ggml-model.sh tiny.en
 make
 make command
+mkdir examples/command2
+wget -O examples/command2/command2.cpp https://github.com/raspberrypisig/whisper.cpp/raw/master/examples/command2/command2.cpp
+cp Makefile Makefile.orig
+sed -ri '/command\:/!{p;d;};n;n;a command2: examples/command2/command2.cpp ggml.o whisper.o\n\t$(CXX) $(CXXFLAGS) examples/command2/command2.cpp ggml.o whisper.o -o command2 $(CC_SDL) $(LDFLAGS)\n' Makefile
+make command2
+
 cat<<EOF > whisper
 #!/usr/bin/env bash
 ./command -m ./models/ggml-tiny.en.bin -ac 768 -t 4 -c 0
 EOF
 chmod +x whisper
+
+cat<<EOF > whisper2
+#!/usr/bin/env bash
+./command2 -m ./models/ggml-tiny.en.bin -ac 768 -t 4 -c 0
+EOF
+chmod +x whisper2
+
+
+
